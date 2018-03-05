2d80b4b46687be37551a4f9718b82feaa927db17
cayeric <cayeric@users.noreply.github.com>
2018-03-05 16:08:46 +0100

    Initial Commit

---
+++ beacontest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beacontest2
+//
+//  Created by Cay-Eric Schimanski on 05.03.18.
+//  Copyright © 2018 endform.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ beacontest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beacontest2
+//
+//  Created by Cay-Eric Schimanski on 05.03.18.
+//  Copyright © 2018 endform.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
